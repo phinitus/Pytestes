fcc45537ce2428d70c8c602d98b8dd18e55b4b30
Caio Ramiro <49010631+phinitus@users.noreply.github.com>
2020-05-04 21:41:34 -0300

    Delete exe04.py

--- EstruturadeDecisao/exe04.py
+++
@@ -1,6 +0,0 @@
-letra = input("Digite uma letra: ")
-letra = letra.upper()
-if letra == "A" or letra == "E" or letra == "I" or letra == "O" or letra == "U":
-    print(f"{letra} é uma vogal.")
-else:
-    print(f"{letra} é uma consoante.")
