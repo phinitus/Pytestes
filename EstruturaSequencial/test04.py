5a235194fad341e9a1676b22bf00b24a405173cb
Caio Ramiro <49010631+phinitus@users.noreply.github.com>
2020-05-04 21:38:44 -0300

    Delete test04.py

--- EstruturaSequencial/test04.py
+++
@@ -1,8 +0,0 @@
-n1 = int(input("Digite a primeira nota bimestral: "))
-n2 = int(input("Digite a segunda nota bimestral: "))
-n3 = int(input("Digite a terceira nota bimestral: "))
-n4 = int(input("Digite a quarta nota bimestral: "))
-
-media = (n1 + n2 + n3 + n4)/ 4
-
-print("A media do aluno é: {}".format(media))
