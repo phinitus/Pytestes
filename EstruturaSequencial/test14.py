b8658c78801f1bd983e2ae6eca2e9c4cbe6dbf8b
Caio Ramiro <49010631+phinitus@users.noreply.github.com>
2020-05-04 21:40:15 -0300

    Delete test14.py

--- EstruturaSequencial/test14.py
+++
@@ -1,4 +0,0 @@
-peso = float(input("Quantos quilos de peixes? "))
-excesso = peso - 50
-multa = excesso * 4
-print(f"Você pescou {peso} kilos de peixe.\nE teve {excesso} kilos de excesso.\nTem que pagar R${multa} de multa.")
