debc11876a4f412f83fe762fc542cd2cd3231bd5
Caio Ramiro <49010631+phinitus@users.noreply.github.com>
2020-05-04 21:41:12 -0300

    Delete exe01.py

--- EstruturadeDecisao/exe01.py
+++
@@ -1,7 +0,0 @@
-num1 = int(input("Digite um número: "))
-num2 = int(input("Digite um número: "))
-
-if num1 > num2:
-    print(f"{num1} é maior que {num2}.")
-else:
-    print(f"{num2} é maior que {num1}.")
